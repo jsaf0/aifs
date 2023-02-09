2a416df13885b2358cf75a996e9c274a1d43e764
Jacob Friis <jacobsafriis@gmail.com>
2023-02-09 21:01:01 +0100

    Type erased awaitable type

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# For macOS, set CC and CXX to gcc-12 and g++-12 respectively.
+# Also remember to update the default Conan profile.
+
 mkdir build && cd build
-conan install .. -pr:b=default -s build_type=Debug
-cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug
+conan install .. -pr:b=default --build=missing -s build_type=Debug
+cmake .. -GNinja -DCMAKE_C_COMPILER=gcc-12 -DCMAKE_CXX_COMPILER=g++-12 
+-DCMAKE_BUILD_TYPE=Debug
 ninja
